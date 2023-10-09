5a0ec23a42fb3d4310f84e33c2a656be33aa3095
Mohammed Abdelgwad <mohammedabdelgwad2000@gmail.com>
2023-10-09 17:03:31 +0300

    first commit

---
+++ lib/Constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class appDesign {
+  static const colorPrimary=Color(0xff213e60);
+  static const colorPrimaryDark=Color(0xff213e60);
+  static const colorPrimaryLight=Color(0xfff6f6f6);
+  static const colorAccent=Color(0xffe68c3e);
+  static const backGround=Color(0xfff6f6f6);
+
+}
